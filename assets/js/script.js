a442b3dc68f387b0d9b2d424bb30998a6d1591c0
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-04-11 18:02:12 -0400

    got form submit function to save locally

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,26 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+const taskTitle = $('#inputTitle');
+const taskDescription = $('#inputDescription');
+const taskDate = $('#inputDate');
+
+
+function formSubmit() {
+    const taskArray = JSON.parse(localStorage.getItem('strings')) || [];
+
+const taskObject = {
+    objectTitle: taskTitle.val(),
+    objectDate: taskDate.val(),
+    objectDescription: taskDescription.val()
+}
+
+taskArray.push(taskObject);
+localStorage.setItem('tasks', JSON.stringify(taskArray));
+console.log(taskArray);
+
+$('#todo-cards').append(createTaskCard(taskObject));
+}
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
@@ -10,7 +30,18 @@ function generateTaskId() {
 // Todo: create a function to create a task card
 function createTaskCard(task) {
 const taskcard = $('<div>')
-    .addClass('card task-card')
+    .addClass('card task-card draggable my-3')
+    // add attribute of task id
+    const cardHeader = $('<div>').addClass('card-header h4').text(task.objectTitle);
+    console.log(task.objectTitle);
+    const cardBody = $('<div>').addClass('card-body');
+    const cardDescription = $('<p>').addClass('card-text').text(task.type);
+    const cardDueDate = $('<p>').addClass('card-text').text(task.dueDate);
+    const cardDeleteBtn = $('<button>')
+    .addClass('btn btn-danger delete')
+    .text('Delete');
+    // project id?
+    cardDeleteBtn.on('click', handleDeleteTask);
 }
 
 // Todo: create a function to render the task list and make cards draggable
@@ -37,3 +68,6 @@ function handleDrop(event, ui) {
 $(document).ready(function () {
 
 });
+
+const formButton = document.getElementById('formButton');
+formButton.addEventListener('click', formSubmit);
